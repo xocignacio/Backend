c7cceb61e409bd900c6487be5c03339577351e8f
xocignacio <xose_ignacio@hotmail.com>
2022-07-20 23:39:27 -0300

    knex

---
+++ Knex/options/mariaDB.js
@@ -0,0 +1,12 @@
+const options = {
+    client: "mysql",
+    connection: {
+      host: "localhost",
+      user: "user",
+      password: "",
+      database: "nombre_db",     /// nombre de la base que creamos en xampp
+    },
+  };
+  
+  module.exports = { options };
+  
